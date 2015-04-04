149c873edf5c580cf2079ca82757b5425b0dfd99
Elte Hupkes <eltehupkes@srxp.com>
2015-04-05 00:01:22 +0200

    Adding posable group

--- posable.py
+++ posable.py
@@ -50,6 +50,14 @@ class Posable(Element):
     Posable is a base class for elements with a name
     and a pose.
     """
+
+    # Whether the pose for this element is in the parent frame
+    # or not. A joint, for instance, has a pose, but it is
+    # expressed in the child link frame. A parent posable
+    # can use this property to decide not to affect this
+    # element with transformations.
+    PARENT_FRAME = True
+
     def __init__(self, name, pose=None, **kwargs):
         """
 
@@ -157,3 +165,78 @@ class Posable(Element):
         rot = self.get_rotation().conjugated()
         pos = self.get_position()
         return rot * (vec - pos)
+
+
+class PosableGroup(Posable):
+    """
+    A PosableGroup allows grouping a set of posables with the same parent
+    without introducing a new coordinate frame (i.e. without putting
+    them inside a link or a model). This lets you conveniently move
+    the items within the group together, whilst their position remains
+    relative to the groups parent.
+    """
+
+    def set_position(self, position: Vector3):
+        """
+        Sets the position of this posable group, translating all the
+        posables within it.
+
+        :param position:
+        :return:
+        """
+        translation = position - self.get_position()
+
+        # Get posables from the element list. It is good practice
+        # to only have posables here, but we can easily make sure.
+        posables = self.get_affected_posables()
+
+        for posable in posables:
+            posable.set_position(posable.get_position() + translation)
+
+        # Store root position
+        super().set_position(position)
+
+    def get_affected_posables(self):
+        """
+        Returns all sub elements which are posable and have their
+        pose in the parent frame.
+        :return:
+        """
+        return [posable for posable in self.elements
+                if isinstance(posable, Posable) and posable.PARENT_FRAME]
+
+    def set_rotation(self, rotation: Quaternion):
+        """
+        Set the rotation of this posable group, moving all the posables
+        within it accordingly.
+
+        :param rotation:
+        :return:
+        """
+        root_position = self.get_position()
+        inv_rotation = self.get_rotation().conjugated()
+
+        posables = self.get_affected_posables()
+        for posable in posables:
+            # Get the position and rotation of the child relative
+            # to this posable (i.e. as if the posable was in [0, 0]
+            # with zero rotation)
+            orig_position = inv_rotation * (posable.get_position() - root_position)
+
+            # The original rotation follows from multiplying the child's rotation
+            # with this group's inverse rotation
+            orig_rotation = inv_rotation * posable.get_rotation()
+
+            # New position means rotating the original point according to the new
+            # rotation, and adding the current position
+            new_position = (rotation * orig_position) + root_position
+
+            # New rotation is acquired by multiplying the new rotation
+            # with the existing rotation
+            new_rotation = rotation * orig_rotation
+
+            posable.set_position(new_position)
+            posable.set_rotation(new_rotation)
+
+        # We should still store our own root rotation
+        super().set_rotation(rotation)
