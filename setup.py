04ec6866353aa3fef43f233e4a3bb24f434e9de3
Elte Hupkes <eltehupkes@srxp.com>
2015-04-08 14:39:02 +0200

    Adding setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='SDF Builder',
+      version=0.1,
+      description='Pragmatic utility library for building SDF files',
+      author='Elte Hupkes',
+      author_email='elte@hupkes.org',
+      url='https://github.com/ElteHupkes/sdf-builder',
+      packages=['sdfbuilder']
+      )
