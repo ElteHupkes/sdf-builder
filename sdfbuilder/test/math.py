bc7de7d9b1a09594a88a8f89fbbe50a500ddc7bd
Elte Hupkes <eltehupkes@srxp.com>
2015-05-08 13:05:00 +0200

    Adding simple math regression test

--- sdfbuilder/test/math.py
+++ sdfbuilder/test/math.py
@@ -9,13 +9,18 @@ class TestMath(unittest.TestCase):
     """
     Tests the math components
     """
+
     def test_euler_angles(self):
         """
-
+        Tests Euler angles from a quaternion that used to fail. Other Euler
+        angles are tested from visual inspection in the `posable` test case.
         :return:
         """
         q = Quaternion(-0.5, 0.5, 0.5, 0.5)
-        print(q.get_rpy())
+        roll, pitch, yaw = q.get_rpy()
+        self.assertAlmostEquals(-1.5707963267948968, roll, msg="Invalid roll.")
+        self.assertAlmostEquals(-1.5707963267948968, pitch, msg="Invalid roll.")
+        self.assertAlmostEquals(0, yaw, msg="Invalid yaw")
 
 
 if __name__ == '__main__':
