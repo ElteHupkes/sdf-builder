e299e3c6162abbbea04c130e78d610ac07f07f41
Elte Hupkes <eltehupkes@srxp.com>
2015-05-21 13:43:12 +0200

    Adding test

--- sdfbuilder/test/element.py
+++ sdfbuilder/test/element.py
@@ -7,6 +7,8 @@ class A(Element):
 class B(Element):
     pass
 
+class C(B):
+    pass
 
 class TestElement(unittest.TestCase):
     def test_filter(self):
@@ -14,17 +16,18 @@ class TestElement(unittest.TestCase):
         sub1 = A()
         sub2 = B()
 
-        sub1a = B()
-        sub1b = A()
+        sub1a = A()
+        sub1b = B()
+        sub1c = C()
 
-        sub1.add_elements([sub1a, sub1b])
+        sub1.add_elements([sub1a, sub1b, sub1c])
         root.add_elements([sub1, sub2])
 
         check = root.get_elements_of_type(B)
         self.assertEquals([sub2], check)
 
         check = root.get_elements_of_type(B, True)
-        self.assertEquals([sub1a, sub2], check)
+        self.assertEquals([sub1b, sub1c, sub2], check)
 
 if __name__ == '__main__':
     unittest.main()
